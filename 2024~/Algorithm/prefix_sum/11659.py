a902bc1f356d128c6546008252395e969b86fed2
HongTorxh <ghdtack@ajou.ac.kr>
2025-07-09 17:03:32 +0900

    [FIX]2025.07.09

--- 2024~/Algorithm/prefix_sum/11659.py
+++ 2024~/Algorithm/prefix_sum/11659.py
@@ -3,11 +3,9 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 nums = list(map(int, input().split()))
-dp = [0] * n
-dp[0] = nums[0]
-
-for i in range(1, n):
-    dp[i] = dp[i-1] + nums[i]
+dp = [0] * (n + 1)
+for i in range(1, n + 1):
+    dp[i] = dp[i-1] + nums[i - 1]
 
 for _ in range(m):
     i, j = map(int, input().split())
